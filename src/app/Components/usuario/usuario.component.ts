a9ac65ad6aeab9d75120457cdbafe3b70f08e8d3
Javier Sánchez <fesain1116@gmail.com>
2023-09-21 18:05:10 -0500

    métodos
    se agregan los demás métodos al controlador de usuarios

--- src/app/Components/usuario/usuario.component.ts
+++ src/app/Components/usuario/usuario.component.ts
@@ -4,17 +4,29 @@ import { RestService } from 'src/app/Services/rest.service';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  styleUrls: ['./usuario.component.css'],
 })
 export class UsuarioComponent implements OnInit {
-
-  constructor(public api: RestService){
-
-  }
+  constructor(public api: RestService) {}
   ngOnInit(): void {
     this.get();
   }
-  public get (){
+  public get() {
     this.api.get('Usuarios');
-  } 
+  }
+  public post() {
+    this.api.post('Usuarios', {
+      Correo: 'jfsanchezi@itc.edu.co',
+      Contraseña: '000000'
+    });
+  }
+  public put() {
+    this.api.put('Usuarios', '1',{
+      Correo: 'jfsanchezi@itc.edu.co',
+      Contraseña: '000000'
+    });
+  }
+  public delete() {
+    this.api.delete('Usuarios', '1');
+  }
 }
--- src/app/Services/rest.service.ts
+++ src/app/Services/rest.service.ts
@@ -14,4 +14,19 @@ export class RestService {
       console.log(res);
     });
   }
+  public async post ( controller: string, text: any){
+    await this.api.post(this.url+controller, text).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
+  public async put ( controller: String, id: string, text: any){
+    await this.api.put(this.url+controller+'/'+id, text).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
+  public async delete ( controller: String, id: string){
+    await this.api.delete(this.url+controller+'/'+id).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
 }

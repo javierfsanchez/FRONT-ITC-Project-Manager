655bf61db7b1b06266efe82ef2fb064ac0bb059d
Javier Sánchez <fesain1116@gmail.com>
2023-10-05 00:43:37 -0500

    Update restservices
    Se actualiza el restservices

--- src/app/Services/rest.service.ts
+++ src/app/Services/rest.service.ts
@@ -7,15 +7,26 @@ import { Injectable } from '@angular/core';
 export class RestService {
 
   constructor(public api: HttpClient) { }
-  url = "https://localhost:7118/api/"
+  url = "https://localhost:7288/api/"
 
   public async get ( controller: String){
-    var result:any
-    await this.api.get(this.url+controller).toPromise().then(x=>{
-      result = x
-      
-    })
-    return result;
-    
+    await this.api.get(this.url+controller).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
+  public async post ( controller: string, text: any){
+    await this.api.post(this.url+controller, text).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
+  public async put ( controller: String, id: string, text: any){
+    await this.api.put(this.url+controller+'/'+id, text).toPromise().then((res)=>{
+      console.log(res);
+    });
+  }
+  public async delete ( controller: String, id: string){
+    await this.api.delete(this.url+controller+'/'+id).toPromise().then((res)=>{
+      console.log(res);
+    });
   }
 }

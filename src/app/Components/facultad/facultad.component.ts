52cdd342b45da19fea28071797105122788229a7
Javier Sánchez <fesain1116@gmail.com>
2023-10-05 00:36:55 -0500

    Revert "Iconos de acción"
    This reverts commit 0cad0b543320c22f9b9faeaeef405e05cf6551c9.

--- src/app/Components/facultad/facultad.component.ts
+++ src/app/Components/facultad/facultad.component.ts
@@ -7,23 +7,7 @@ import { RestService } from 'src/app/Services/rest.service';
   styleUrls: ['./facultad.component.css']
 })
 
-<<<<<<< Updated upstream
 export class FacultadComponent implements OnInit {
-=======
-export class FacultadComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = [];
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  dataSource: MatTableDataSource<any>;
-  constructor(public api: RestService){
-  this.dataSource = new MatTableDataSource();
-  }
-  ngOnInit(): void {
-    this.api.get('Facultades').then((res)=>{
-    for (let index = 0; index < res.length; index++){
-      this.loadTable([res[index]])
-    }
->>>>>>> Stashed changes
 
     constructor(public api: RestService){
   

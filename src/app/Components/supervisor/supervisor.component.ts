2171c42019748e91ea4a84794e5d357f10a9f11d
xHarald <jrreinosoronaldo@gmail.com>
2023-11-16 20:14:19 -0500

    Actualización de tablas
    Cambios realizados al Front

--- src/app/Components/supervisor/supervisor.component.ts
+++ src/app/Components/supervisor/supervisor.component.ts
@@ -1,8 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator} from '@angular/material/paginator';
+import { MatSort} from '@angular/material/sort';
+import { MatTableDataSource} from '@angular/material/table';
+import { FromSupervisorComponent } from 'src/app/Forms/from-supervisor/from-supervisor.component';
 import { RestService } from 'src/app/Services/rest.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-supervisor',
@@ -11,46 +14,98 @@ import { RestService } from 'src/app/Services/rest.service';
 })
 
 export class SupervisorComponent implements OnInit, AfterViewInit {
-    displayedColumns: string[] = [];
+    displayedColumns: string[] = [];  
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
     dataSource: MatTableDataSource<any>;
-    constructor(public api: RestService){
-    this.dataSource = new MatTableDataSource();
+
+    constructor(public api: RestService, public dialog: MatDialog){
+      this.dataSource = new MatTableDataSource();
     }
+
     ngOnInit(): void {
+      this.get();
+      
+    }
+    
+    public get(){
       this.api.get('Supervisores').then((res)=>{
       for (let index = 0; index < res.length; index++){
         this.loadTable([res[index]])
       }
-  
-     this.dataSource.data=res
-      
-  
-      console.log(res);
-  
+      this.dataSource.data=res
+        console.log(res);
       })
     }
-  
+
+    public deleteRegistro(id: number) {
+      console.log(id);
+      Swal.fire({
+          title: '¿Está seguro eliminar el registro?',
+          text: "No podrá revertir esta operación.",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#5f5ffd',
+          cancelButtonColor: '#ff5e5e',
+          confirmButtonText: 'Eliminar'
+      }).then((result) => {
+          if (result.isConfirmed) {
+              this.api.delete('Supervisores', id.toString(), { activo: false }) // Llama al método Put para marcar el estudiante como inactivo
+                  .then(() => {
+                      Swal.fire(
+                          'Eliminado!',
+                          `El registro con el id ${id} ha sido eliminado.`,
+                          'success'
+                      );
+                      setInterval(() => {
+                          window.location.reload();
+                      }, 2000);
+                  })
+                  .catch((error) => {
+                      console.error(error);
+                      // Manejar el error aquí
+                  });
+          }
+      });
+      return false;
+  }
+
+    // deleteService(id){
+    //   this.api.delete("Estudiantes/", id);
+    // }
+
+    public editarRegistro(index: number){
+      alert("Editando el registro");
+    }
+    
     ngAfterViewInit(): void {
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-  
+      this.paginator._intl.itemsPerPageLabel = 'Registros por página';
     }
     
     loadTable(data:any[]){
       this.displayedColumns=[];
-      for(let column in data[0]){
-        this.displayedColumns.push(column)
+      if (data.length > 0) {
+        for(let column in data[0]){
+          this.displayedColumns.push(column);
+        }
+        this.displayedColumns.push("Acciones")
       }
     }
-  
+    
     applyFilter(event: Event) {
       const filterValue = (event.target as HTMLInputElement).value;
       this.dataSource.filter = filterValue.trim().toLowerCase();
-  
+
       if (this.dataSource.paginator) {
         this.dataSource.paginator.firstPage();
       }
+    }
+
+    openDialog() {
+      this.dialog.open(FromSupervisorComponent, {
+        width: '50%'
+      });
     }
   }
--- src/app/Forms/from-programa/from-programa.component.ts
+++ src/app/Forms/from-programa/from-programa.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 
 import { FormBuilder, Validators } from '@angular/forms';
+import { FormsService } from 'src/app/Services/forms.service';
 import Swal from 'sweetalert2';
 
 
@@ -9,7 +10,7 @@ import Swal from 'sweetalert2';
   templateUrl: './from-programa.component.html',
   styleUrls: ['./from-programa.component.css']
 })
-export class FromProgramaComponent {
+export class FromProgramaComponent implements OnInit {
   private fb = inject(FormBuilder);
   formularioProgramas = this.fb.group({
     program: [null, Validators.required],
@@ -17,6 +18,22 @@ export class FromProgramaComponent {
     facultad: [null, Validators.required],
        
   });
+
+
+  constructor(public FormService:FormsService) {
+  
+  
+}
+title:string;
+  ngOnInit(): void {
+    if(this.FormService.title=='Editar'){
+      this.title='Editar';
+
+    } else if(this.FormService.title=='Crear'){
+      this.title='Crear';
+
+    }
+  }
 
   onSubmit(): void {
     if(this.formularioProgramas.valid){

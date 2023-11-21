5050e802ab6fb599d0ef0560011930d79f9fdcca
xHarald <jrreinosoronaldo@gmail.com>
2023-11-20 20:16:28 -0500

    Editar registros
    editar registros en el front

--- src/app/Forms/from-programa/from-programa.component.ts
+++ src/app/Forms/from-programa/from-programa.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Programas } from 'src/app/Models/Programas';
 import { FormsService } from 'src/app/Services/forms.service';
+import { RestService } from 'src/app/Services/rest.service';
 import Swal from 'sweetalert2';
 
 
@@ -13,14 +15,14 @@ import Swal from 'sweetalert2';
 export class FromProgramaComponent implements OnInit {
   private fb = inject(FormBuilder);
   formularioProgramas = this.fb.group({
-    program: [null, Validators.required],
-    descripcion: [null, Validators.required],
-    facultad: [null, Validators.required],
+    program: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    facultad: ['', Validators.required],
        
   });
 
 
-  constructor(public FormService:FormsService) {
+  constructor(public FormService:FormsService, public api:RestService) {
   
   
 }
@@ -28,7 +30,9 @@ title:string;
   ngOnInit(): void {
     if(this.FormService.title=='Editar'){
       this.title='Editar';
-
+      this.formularioProgramas.setControl('program', new FormControl(this.FormService.Programas.nombre_Programa));
+      this.formularioProgramas.setControl('descripcion', new FormControl(this.FormService.Programas.descripcion));
+      this.formularioProgramas.setControl('facultad', new FormControl(this.FormService.Programas.facultad));
     } else if(this.FormService.title=='Crear'){
       this.title='Crear';
 
@@ -37,6 +41,19 @@ title:string;
 
   onSubmit(): void {
     if(this.formularioProgramas.valid){
+      if(this.FormService.title=='Editar'){
+        let object:Programas = {
+          id:Number(this.FormService.Programas.codigo),
+          nombre_Programa:this.formularioProgramas.controls['program'].value,
+          descripcion:this.formularioProgramas.controls['descripcion'].value,
+          facultad:this.formularioProgramas.controls['facultad'].value,
+          estado:this.FormService.Programas.estado       
+
+        }
+        this.api.put('Programas', this.FormService.Programas.codigo.toString(), object);
+
+
+      }
       Swal.fire('Felicidades','Registro exitoso','success')
     }else {
       Swal.fire('Peligro','Registro incorrecto','error')
